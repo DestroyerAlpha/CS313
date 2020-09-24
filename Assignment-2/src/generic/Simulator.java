789f97b478a3f52cc06cc9ebd6e1fa9d873a07ab
Brinal Savsaviya <55130735+brinal8055@users.noreply.github.com>
2020-09-24 12:58:50 +0530

    Update Simulator.java

--- Assignment-2/src/generic/Simulator.java
+++ Assignment-2/src/generic/Simulator.java
@@ -4,6 +4,8 @@ import java.io.*;
 import java.util.*;
 import java.io.FileInputStream;
 import generic.Operand.OperandType;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 
 
 public class Simulator {
@@ -19,7 +21,9 @@ public class Simulator {
 	
 	public static void assemble(String objectProgramFile)
 	{
-		int stack_top = 65535;
+		File file = new File("foo.out");
+
+		int stack_top = 65536;
 		System.out.println(ParsedProgram.symtab.get("main"));
 		for(int i=0;i<ParsedProgram.data.size();i++)
 		{
@@ -30,37 +34,156 @@ public class Simulator {
 			// System.out.println(ParsedProgram.getInstructionAt(i).getProgramCounter());
 			String instruction = Integer.toBinaryString(instruction_code(ParsedProgram.getInstructionAt(i).getOperationType()));
 			instruction = String.format("%5s", instruction).replaceAll(" ", "0");
-			if(ParsedProgram.getInstructionAt(i).getSourceOperand1()!=null)
-			{
-				if(ParsedProgram.getInstructionAt(i).getSourceOperand1().operandType==Operand.OperandType.Register)
-				{
-					String op = Integer.toBinaryString(ParsedProgram.getInstructionAt(i).getSourceOperand1().getValue());
-					op = String.format("%5s", op).replaceAll(" ", "0");
-					instruction += op;
-				}
-				else if(ParsedProgram.getInstructionAt(i).getSourceOperand1().operandType==Operand.OperandType.Label)
-				{
-					int addr = stack_top - ParsedProgram.symtab.get(ParsedProgram.getInstructionAt(i).getSourceOperand1().getLabelValue());
 
-				}
-				
-			}
-			else
+			switch(ParsedProgram.getInstructionAt(i).getOperationType())
 			{
-				instruction += "00000";
+			//R3I type
+			case add : 
+			case sub : 
+			case mul : 
+			case div : 
+			case and : 
+			case or : 
+			case xor : 
+			case slt : 
+			case sll : 
+			case srl : 
+			case sra :	{
+							String op = Integer.toBinaryString(ParsedProgram.getInstructionAt(i).getSourceOperand1().getValue());
+							op = String.format("%5s", op).replaceAll(" ", "0");
+							instruction += op;	//rs1 bits
+							
+							String op = Integer.toBinaryString(ParsedProgram.getInstructionAt(i).getSourceOperand2().getValue());
+							op = String.format("%5s", op).replaceAll(" ", "0");
+							instruction += op;	//rs2 bits
+
+							String op = Integer.toBinaryString(ParsedProgram.getInstructionAt(i).getDestinationOperand().getValue());
+							op = String.format("%5s", op).replaceAll(" ", "0");
+							instruction += op;	//rd bits
+
+							instruction += "000000000000";	//unused bits in machine code is set of all zeros :)
+							break;
+						} 
+			
+			//R2I type
+			case addi :
+			case subi :
+			case muli :
+			case divi : 
+			case andi : 
+			case ori : 
+			case xori : 
+			case slti : 
+			case slli : 
+			case srli : 
+			case srai :
+			case load :
+			case store :	{
+								String op = Integer.toBinaryString(ParsedProgram.getInstructionAt(i).getSourceOperand1().getValue());
+								op = String.format("%5s", op).replaceAll(" ", "0");
+								instruction += op;	//rs1 bits
+
+								String op = Integer.toBinaryString(ParsedProgram.getInstructionAt(i).getDestinationOperand().getValue());
+								op = String.format("%5s", op).replaceAll(" ", "0");
+								instruction += op;	//rd bits
+
+								if(ParsedProgram.getInstructionAt(i).getSourceOperand1().operandType==Operand.OperandType.Immediate)
+								{
+									String op = Integer.toBinaryString(ParsedProgram.getInstructionAt(i).getSourceOperand2().getValue());
+									op = String.format("%17s", op).replaceAll(" ", "0");
+									instruction += op;	//absolute immediate
+								}
+								else
+								{
+									String op = Integer.toBinaryString(ParsedProgram.symtab.get(ParsedProgram.getInstructionAt(i).getSourceOperand2().getLabelValue()));
+									op = String.format("%17s", op).replaceAll(" ", "0");
+									instruction += op;	//label / symbol
+								}
+
+								break;
+							} 
+			
+			case beq : 
+			case bne : 
+			case blt : 
+			case bgt : 	{
+							String op = Integer.toBinaryString(ParsedProgram.getInstructionAt(i).getSourceOperand1().getValue());
+							op = String.format("%5s", op).replaceAll(" ", "0");
+							instruction += op;	//rs1 bits
+
+							String op = Integer.toBinaryString(ParsedProgram.getInstructionAt(i).getDestinationOperand().getValue());
+							op = String.format("%5s", op).replaceAll(" ", "0");
+							instruction += op;	//rd bits
+
+							if(ParsedProgram.getInstructionAt(i).getSourceOperand1().operandType==Operand.OperandType.Immediate)
+							{
+								String op = Integer.toBinaryString(ParsedProgram.getInstructionAt(i).getSourceOperand2().getValue());
+								op = String.format("%17s", op).replaceAll(" ", "0");
+								instruction += op;	//absolute immediate
+							}
+							else
+							{
+								String op = Integer.toBinaryString(ParsedProgram.symtab.get(ParsedProgram.getInstructionAt(i).getSourceOperand2().getLabelValue()));
+								op = String.format("%17s", op).replaceAll(" ", "0");
+								instruction += op;	//label / symbol
+							}
+
+							break;
+						}
+			
+			//RI type :
+			case jmp :		{
+								if(ParsedProgram.getInstructionAt(i).getDestinationOperand().operandType==Operand.OperandType.Register)
+								{
+									String op = Integer.toBinaryString(ParsedProgram.getInstructionAt(i).getDestinationOperand().getValue());
+									op = String.format("%5s", op).replaceAll(" ", "0");
+									instruction += op;	//rd bits
+
+									instruction += "0000000000000000000000";
+								}
+								else if(ParsedProgram.getInstructionAt(i).getDestinationOperand().operandType==Operand.OperandType.Label)
+								{
+									instruction += "00000";
+
+									String op = Integer.toBinaryString(ParsedProgram.symtab.get(ParsedProgram.getInstructionAt(i).getDestinationOperand().getLabelValue()));
+									op = String.format("%17s", op).replaceAll(" ", "0");
+									instruction += op;	//imm
+								}
+								else if(ParsedProgram.getInstructionAt(i).getDestinationOperand().operandType==Operand.OperandType.Immediate)
+								{
+									instruction += "00000";
+
+									String op = Integer.toBinaryString(ParsedProgram.getInstructionAt(i).getDestinationOperand().getValue());
+									op = String.format("%17s", op).replaceAll(" ", "0");
+									instruction += op;	//label / symbol
+								}
+
+								break;
+							}
+			
+			case end :
+						{
+							instruction += "000000000000000000000000000";
+							break;
+						}
 			}
-			if(ParsedProgram.getInstructionAt(i).getSourceOperand2()!=null)
-			{
-				String op = Integer.toBinaryString(ParsedProgram.getInstructionAt(i).getSourceOperand2().getValue());
-				op = String.format("%5s", op).replaceAll(" ", "0");
-				instruction += op;
+
+			String result = binaryUnicodeToString(instruction);
+			System.out.println(result);
+			System.out.println(result.trim());
+			
+			try (FileOutputStream fos = new FileOutputStream(file);
+					BufferedOutputStream bos = new BufferedOutputStream(fos);
+					DataOutputStream dos = new DataOutputStream(bos)) {
+				dos.writeBytes(result);
+				System.out.println("Successfully written data to the file");
 			}
-			else
-			{
-				instruction += "00000";
+			catch (IOException e) {
+				e.printStackTrace();
 			}
-			System.out.println(ParsedProgram.getInstructionAt(i).getDestinationOperand().getLabelValue());
-		}
+			
+		}	
+
 		// System.out.println(ParsedProgram.symtab.get("loop"));
 		
 		
@@ -70,43 +193,58 @@ public class Simulator {
 		//3. write the data to the file
 		//4. assemble one instruction at a time, and write to the file
 		//5. close the file
-	}
 
-	public static int instruction_code(Instruction.OperationType in)
-	{
-		ArrayList<Instruction.OperationType> ins = new ArrayList<Instruction.OperationType>();
-		for(Instruction.OperationType o: Instruction.OperationType.values())
+		public static String binaryUnicodeToString(String binary)
 		{
-			ins.add(o);
+
+			System.out.println("2435654345676678");
+			System.out.println(Long.parseLong(binary, 2));
+			System.out.println("2435654345676678");
+			byte[] array = ByteBuffer.allocate(4).putInt(
+					Integer.parseInt(binary, 2)
+			).array();
+	
+			return new String(array, StandardCharsets.UTF_8);
 		}
-		for(int i=0;i<ins.size();i++)
+
+		public static int instruction_code(Instruction.OperationType in)
 		{
-			if(ins.get(i)==in)
+			ArrayList<Instruction.OperationType> ins = new ArrayList<Instruction.OperationType>();
+			for(Instruction.OperationType o: Instruction.OperationType.values())
 			{
-				return i;
+				ins.add(o);
 			}
+			for(int i=0;i<ins.size();i++)
+			{
+				if(ins.get(i)==in)
+				{
+					return i;
+				}
+			}
+			return -1;
 		}
-		return -1;
-	}
-	public static byte[] toBinary5bit(int number) { 
-		byte[] binary = new byte[5]; 
-		int index = 0; 
-		int copyOfInput = number; 
-		while (copyOfInput > 0) { 
-			binary[index++] = (byte) (copyOfInput % 2); 
-			copyOfInput = copyOfInput / 2; 
+
+		public static byte[] toBinary5bit(int number) { 
+			byte[] binary = new byte[5]; 
+			int index = 0; 
+			int copyOfInput = number; 
+			while (copyOfInput > 0) { 
+				binary[index++] = (byte) (copyOfInput % 2); 
+				copyOfInput = copyOfInput / 2; 
+			}
+			return binary; 
 		}
-		return binary; 
-	}
-	public static byte[] toBinary(int number) { 
-		byte[] binary = new byte[32]; 
-		int index = 0; 
-		int copyOfInput = number; 
-		while (copyOfInput > 0) { 
-			binary[index++] = (byte) (copyOfInput % 2); 
-			copyOfInput = copyOfInput / 2; 
+		
+		public static byte[] toBinary(int number) { 
+			byte[] binary = new byte[32]; 
+			int index = 0; 
+			int copyOfInput = number; 
+			while (copyOfInput > 0) { 
+				binary[index++] = (byte) (copyOfInput % 2); 
+				copyOfInput = copyOfInput / 2; 
+			}
+			return binary; 
 		}
-		return binary; 
 	}
 	
 }
