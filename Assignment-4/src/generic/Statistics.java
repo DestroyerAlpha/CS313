d8d89b44b4c4d96763409eaea9914d622f9b5ec3
Brinal Savsaviya <55130735+brinal8055@users.noreply.github.com>
2020-10-28 15:45:40 +0530

    Add files via upload

---
+++ Assignment-4/src/generic/Statistics.java
@@ -0,0 +1,40 @@
+package generic;
+
+import java.io.PrintWriter;
+
+public class Statistics {
+	
+	// TODO add your statistics here
+	public static int numberOfInstructions = 0;
+	public static int numberOfCycles = 0;
+	
+
+	public static void printStatistics(String statFile)
+	{
+		try
+		{
+			PrintWriter writer = new PrintWriter(statFile);
+			
+			writer.println("Number of instructions executed = " + numberOfInstructions);
+			writer.println("Number of cycles taken = " + numberOfCycles);
+			
+			writer.close();
+		}
+		catch(Exception e)
+		{
+			Misc.printErrorAndExit(e.getMessage());
+		}
+	}
+	
+	public void setNumberOfInstructions(int numberOfInstructions) {
+		Statistics.numberOfInstructions = numberOfInstructions;
+	}
+
+	public void setNumberOfCycles(int numberOfCycles) {
+		Statistics.numberOfCycles = numberOfCycles;
+	}
+
+	public int getNumberOfInstructions() {
+		return Statistics.numberOfInstructions;
+	}
+}
